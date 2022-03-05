3d400b00819ef81c77b670fccca19a51d1978bd3
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-05 20:46:14 +0530

    employee_wage

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,12 +1,20 @@
 ﻿Console.WriteLine("Welcome to employee wage computation");
 int present = 1;
+int Emp_per_rate = 20;
+int emphr = 0;
+int empwage = 0;
 Random random = new Random();
 int Employeestates = random.Next(0, 2);
 if (Employeestates == present)
 {
     Console.WriteLine("the Employee is present");
+    emphr = 8;
 }
 else
 {
     Console.WriteLine("the Employee is absent");
+
+    emphr = 0;
 }
+empwage = (Emp_per_rate * emphr);
+Console.WriteLine("Daily wage" + empwage);
