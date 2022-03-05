9d47e5c55ddbdfc770fa179c7e41faddc2e615ef
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-05 20:40:13 +0530

    emp_attendance

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,1 +1,12 @@
 ﻿Console.WriteLine("Welcome to employee wage computation");
+int present = 1;
+Random random = new Random();
+int Employeestates = random.Next(0, 2);
+if (Employeestates == present)
+{
+    Console.WriteLine("the Employee is present");
+}
+else
+{
+    Console.WriteLine("the Employee is absent");
+}
