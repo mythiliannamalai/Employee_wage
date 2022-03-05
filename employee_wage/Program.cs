ec4765e051ea9192e9a515c9cce0b2969623bda6
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-05 20:52:49 +0530

    emp_fulltime_partime

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -1,14 +1,25 @@
 ﻿Console.WriteLine("Welcome to employee wage computation");
 int present = 1;
+int fulltime = 1;
 int Emp_per_rate = 20;
 int emphr = 0;
 int empwage = 0;
 Random random = new Random();
+Random random1 = new Random();
 int Employeestates = random.Next(0, 2);
+int Employeetime = random1.Next(0, 2);
 if (Employeestates == present)
 {
-    Console.WriteLine("the Employee is present");
-    emphr = 8;
+    if (Employeetime == fulltime)
+    {
+        Console.WriteLine("the Employee is present");
+        emphr = 8;
+    }
+    else
+    {
+        Console.WriteLine("the Employee is partimer");
+        emphr = 4;
+    }
 }
 else
 {
