8c7bf5625ec3cf7fddd2578699b6593768f2a07e
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-06 19:29:20 +0530

    working_hr_days

--- employee_wage/Program.cs
+++ employee_wage/Program.cs
@@ -3,25 +3,31 @@ int present = 1;
 int fulltime = 1;
 int Emp_per_rate = 20;
 int emphr = 0;
+int emphrs = 0;
 int empwage = 0;
 int total_wday = 20;
-int month_pay=0;
-Random random = new Random();
-Random random1 = new Random();
-int Employeestates = random.Next(0, 2);
-int Employeetime = random1.Next(0, 2);
-if (Employeestates == present)
+int month_pay = 0;
+int empattc=0;
+while (empattc != 20 && emphrs != 100)
 {
-    present = 1;
-}
-else
-{
-    present = 0;
+    Random random = new Random();
+    Random random1 = new Random();
+    int Employeestates = random.Next(0, 2);
+    int Employeetime = random1.Next(0, 2);
+    if (Employeestates == present)
+    {
+        present = 1;
+    }
+    else
+    {
+        present = 0;
 
-}
+    }
+
     switch (present)
     {
         case 1:
+            empattc++;
             if (Employeetime == fulltime)
             {
                 Console.WriteLine("the Employee is present");
@@ -39,7 +45,11 @@ else
             emphr = 0;
             break;
     }
+
+    emphrs = (emphr + emphrs);
+}
 empwage = (Emp_per_rate * emphr);
 Console.WriteLine("Daily wage" + empwage);
 month_pay = (total_wday * empwage);
 Console.WriteLine("monthly wage : " + month_pay);
+Console.WriteLine("working hours :" + emphrs);
